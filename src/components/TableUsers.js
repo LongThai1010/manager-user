6409955eddb41c1d6943fbb2d9bb369219fb0a1e
long thai <volelongthaipnt@gmail.com>
2023-07-02 22:25:00 +0700

    pagination for list-user

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -2,22 +2,32 @@ import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import { fetchAllUser } from "../services/UserService";
+import ReactPaginate from "react-paginate";
 
 function TableUsers() {
   const [listUser, setListUser] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    getUsers();
+    getUsers(1);
   }, []);
 
-  const getUsers = async () => {
-    let res = await fetchAllUser();
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
 
     if (res && res.data && res.data) {
       setListUser(res.data);
+      setTotalUsers(res.total);
+      setTotalPages(res.total_pages);
     }
   };
   console.log(listUser);
+
+  const handlePageClick = (event) => {
+    console.log(event);
+    getUsers(+event.selected + 1);
+  };
 
   return (
     <Container>
@@ -45,6 +55,26 @@ function TableUsers() {
             })}
         </tbody>
       </Table>
+      <ReactPaginate
+        previousLabel="Previous"
+        nextLabel="Next"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        pageCount={totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
+        activeClassName="active"
+        forcePage={10}
+      />
     </Container>
   );
 }

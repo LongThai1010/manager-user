bd1eef5c0ac97f097f9bc629d722547522671dd6
long thai <volelongthaipnt@gmail.com>
2023-07-02 21:52:07 +0700

    Customize axios

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserService";
+
 function TableUsers() {
-    return ( <> TableUsers</> );
+  const [listUser, setListUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+
+    if (res && res.data && res.data) {
+      setListUser(res.data);
+    }
+  };
+  console.log(listUser);
+
+  return (
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUser &&
+            listUser.length > 0 &&
+            listUser.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
 export default TableUsers;
